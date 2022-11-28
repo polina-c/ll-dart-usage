769da2938b2211e5e547e7eaea01ba1fd2a62236
Polina Cherkasova <polinach@google.com>
2022-11-27 23:05:52 -0800

    -

---
+++ bin/ll_dart_usage.dart
@@ -0,0 +1,5 @@
+import 'package:ll_dart_usage/ll_dart_usage.dart' as ll_dart_usage;
+
+void main(List<String> arguments) {
+  print('Hello world: ${ll_dart_usage.calculate()}!');
+}
---
+++ test/ll_dart_usage_test.dart
@@ -0,0 +1,8 @@
+import 'package:ll_dart_usage/ll_dart_usage.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
